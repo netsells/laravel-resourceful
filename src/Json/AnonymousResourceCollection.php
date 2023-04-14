c4c7436aaf31c42d2b290c9628d6e1da3415230e
Jakub Gawron <jakub.gawron@netsells.co.uk>
2023-04-14 16:40:54 +0100

    Fixes to keep the codebase working on Laravel 9, but make it ready for Laravel 10.

--- src/Json/AnonymousResourceCollection.php
+++ src/Json/AnonymousResourceCollection.php
@@ -4,6 +4,13 @@ namespace Netsells\Http\Resources\Json;
 
 class AnonymousResourceCollection extends ResourceCollection
 {
+    /**
+     * Indicates if the collection keys should be preserved.
+     *
+     * @var bool
+     */
+    public $preserveKeys = false;
+
     public function __construct(mixed $resource, string $collects)
     {
         $this->collects = $collects;
--- src/Json/JsonResource.php
+++ src/Json/JsonResource.php
@@ -12,6 +12,18 @@ class JsonResource extends \Illuminate\Http\Resources\Json\JsonResource
     use ResolvesResources;
     use ResolvesEloquentResources;
 
+    /**
+     * @deprecated This method can be removed when dropping support for Laravel 9.
+     */
+    public static function collection($resource)
+    {
+        return tap(static::newCollection($resource), function ($collection) { // @phpstan-ignore-line
+            if (property_exists(static::class, 'preserveKeys')) {
+                $collection->preserveKeys = (new static([]))->preserveKeys === true; // @phpstan-ignore-line
+            }
+        });
+    }
+
     protected function beforeResolveRoot(Request $request): void
     {
         $this->collectAndResolvePreloads($request, Collection::make([$this]));
